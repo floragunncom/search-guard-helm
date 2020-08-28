e24131833d346d3c6b5ecdc5b6f7b0e49097f9cc
Lyudmila Huzenko <lhuzenko@search-guard.com>
2020-08-28 15:21:09 +0300

    Moving sgadmin to jobs, updating versions etc.

--- init_helm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl create serviceaccount --namespace kube-system tiller
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-#kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-helm init --wait --service-account tiller --upgrade
