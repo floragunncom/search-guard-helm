eaf59818b6901d40958019559e66a637b85d07e6
Piotr Chmielnik <piotr.chmielnik@eliatra.com>
2024-09-27 18:56:41 +0200

    Use storage-provisioner-rancher for minikube

--- tests/prepare_minikube.sh
+++ tests/prepare_minikube.sh
@@ -14,12 +14,7 @@ minikube delete -p "$PROFILE"
 set -e
 minikube start --kubernetes-version "$VERSION" --nodes 3 -p "$PROFILE" --wait=true
 
-#fix minikube issues with hostpath permissions on multicluster nodes
-#https://github.com/kubernetes/minikube/issues/12165
-#https://stackoverflow.com/questions/60479594/minikube-volume-write-permissions
-curl -Ss https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml | sed 's/\/opt\/local-path-provisioner/\/var\/opt\/local-path-provisioner/ ' | kubectl apply -f -
-kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
-kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+minikube  -p "$PROFILE" addons enable storage-provisioner-rancher
 minikube -p "$PROFILE" addons enable metrics-server
 minikube dashboard -p "$PROFILE" &
 
