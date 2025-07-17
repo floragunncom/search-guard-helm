0f5a118784a4ea9969f493d13ede5ae54f692805
DevOps Automat <devops@automat.berlin>
2025-07-17 12:34:17 +0000

    Release 8.18.x

--- tests/test_different_k8s_versions.sh
+++ tests/test_different_k8s_versions.sh
@@ -47,30 +47,30 @@ do
   echo "############################################" 
   echo "$0 Created the k8s env for $version"
   echo "############################################"
-  echo ""
-  echo ""
-  echo ""
-  echo "############################################" 
-  echo "$0 Running the test for ELK 7 for kubernetes $version"
-  echo "############################################"
-  ./run_tests_elk_7.sh  common.elkversion="7.17.18",common.sgversion="1.6.0-flx",common.sgkibanaversion="1.6.0-flx",common.sgctl_version="1.6.0"  false  
-  ./run_tests_elk_7.sh  common.elkversion="7.17.17",common.sgversion="1.4.1-flx",common.sgkibanaversion="1.4.1-flx",common.sgctl_version="1.4.0"  false
-  ./run_tests_elk_7.sh  common.elkversion="7.17.15",common.sgversion="1.4.0-flx",common.sgkibanaversion="1.4.0-flx",common.sgctl_version="1.4.0"  false 
-  ./run_tests_elk_7.sh  common.elkversion="7.17.14",common.sgversion="1.4.0-flx",common.sgkibanaversion="1.4.0-flx",common.sgctl_version="1.4.0"  false          
-  echo "############################################" 
-  echo "$0 Finished tests ELK 7 for kubernetes $version"
-  echo "############################################"
-  echo ""
-  echo ""
-  echo ""
+  # echo ""
+  # echo ""
+  # echo ""
+  # echo "############################################" 
+  # echo "$0 Running the test for ELK 7 for kubernetes $version"
+  # echo "############################################"
+  # ./run_tests_elk_7.sh  common.elkversion="7.17.18",common.sgversion="1.6.0-flx",common.sgkibanaversion="1.6.0-flx",common.sgctl_version="1.6.0"  false  
+  # ./run_tests_elk_7.sh  common.elkversion="7.17.17",common.sgversion="1.4.1-flx",common.sgkibanaversion="1.4.1-flx",common.sgctl_version="1.4.0"  false
+  # ./run_tests_elk_7.sh  common.elkversion="7.17.15",common.sgversion="1.4.0-flx",common.sgkibanaversion="1.4.0-flx",common.sgctl_version="1.4.0"  false 
+  # ./run_tests_elk_7.sh  common.elkversion="7.17.14",common.sgversion="1.4.0-flx",common.sgkibanaversion="1.4.0-flx",common.sgctl_version="1.4.0"  false          
+  # echo "############################################" 
+  # echo "$0 Finished tests ELK 7 for kubernetes $version"
+  # echo "############################################"
+  # echo ""
+  # echo ""
+  # echo ""
   echo "############################################" 
   echo "$0 Running the test for ELK 8 for kubernetes $version"
   echo "############################################"
-  ./run_tests.sh  common.elkversion="8.12.2",common.sgversion="2.0.0-flx",common.sgkibanaversion="2.0.0-flx",common.sgctl_version="2.0.0"  false
-  ./run_tests.sh  common.elkversion="8.11.4",common.sgversion="2.0.0-flx",common.sgkibanaversion="2.0.0-flx",common.sgctl_version="2.0.0"  false
-  ./run_tests.sh  common.elkversion="8.10.4",common.sgversion="2.0.0-flx",common.sgkibanaversion="2.0.0-flx",common.sgctl_version="2.0.0"  false
-  ./run_tests.sh  common.elkversion="8.9.2",common.sgversion="2.0.0-flx",common.sgkibanaversion="2.0.0-flx",common.sgctl_version="2.0.0"  false
-  ./run_tests.sh  common.elkversion="8.8.2",common.sgversion="2.0.0-flx",common.sgkibanaversion="2.0.0-flx",common.sgctl_version="2.0.0"  false
+  ./run_tests.sh  common.elkversion="8.18.3",common.sgversion="3.1.0-flx",common.sgkibanaversion="3.1.0-flx",common.sgctl_version="3.1.0"  false
+  ./run_tests.sh  common.elkversion="8.18.2",common.sgversion="3.1.0-flx",common.sgkibanaversion="3.1.0-flx",common.sgctl_version="3.1.0"  false
+  ./run_tests.sh  common.elkversion="8.18.2",common.sgversion="3.1.0-flx",common.sgkibanaversion="3.1.0-flx",common.sgctl_version="3.1.0"  false
+  ./run_tests.sh  common.elkversion="8.18.1",common.sgversion="3.1.0-flx",common.sgkibanaversion="3.1.0-flx",common.sgctl_version="3.1.0"  false
+  ./run_tests.sh  common.elkversion="8.18.0",common.sgversion="3.1.0-flx",common.sgkibanaversion="3.1.0-flx",common.sgctl_version="3.1.0"  false  
       
   echo ""
   echo ""
