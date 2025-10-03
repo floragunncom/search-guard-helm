affedb72fa9a65601a1150a9fcd75006bb5e0355
Bart Bubak <bartosz.bubak@eliatra.com>
2025-10-03 14:21:18 +0200

    Labeling Bufix

--- examples/common/custom_secrets/tests/pre_upgrade.sh
+++ examples/common/custom_secrets/tests/pre_upgrade.sh
@@ -4,6 +4,8 @@ set -x
 #$2 - yaml files folder
 
 # Create test secrets
-kubectl create secret generic common-custom-secret -n $1 --from-literal=secret=CommonSuperSecret --from-literal=password=CommonSecretPassword -l app=sg-elk-search-guard-flx
-kubectl create secret generic kibana-custom-secret -n $1 --from-literal=secret=KibanaSuperSecret --from-literal=password=KibanaSecretPassword -l app=sg-elk-search-guard-flx
+kubectl create secret generic common-custom-secret -n $1 --from-literal=secret=CommonSuperSecret --from-literal=password=CommonSecretPassword 
+kubectl label secret common-custom-secret app=sg-elk-search-guard-flx -n $1
+kubectl create secret generic kibana-custom-secret -n $1 --from-literal=secret=KibanaSuperSecret --from-literal=password=KibanaSecretPassword 
+kubectl label secret kibana-custom-secret app=sg-elk-search-guard-flx -n $1
 
