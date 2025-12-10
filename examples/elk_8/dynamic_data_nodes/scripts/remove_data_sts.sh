d56ac76902d9d6e2881573461f39fde69b84a539
Devops Automat <devops@automat.berlin>
2025-12-10 10:26:00 +0100

    Use dynamic for data content

--- examples/elk_8/dynamic_data_nodes/scripts/remove_data_sts.sh
+++ examples/elk_8/dynamic_data_nodes/scripts/remove_data_sts.sh
@@ -4,3 +4,5 @@ set -x
 #$2 - yaml files folder
 kubectl -n $1 delete sts -l role=data
 kubectl -n $1 wait --for=delete pod -l role=data --timeout=300s
+kubectl -n $1 delete sts -l role=data-content
+kubectl -n $1 wait --for=delete pod -l role=data-content --timeout=300s
