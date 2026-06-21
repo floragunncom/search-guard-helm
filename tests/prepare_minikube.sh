74acb08c30b9e54784e745d0bce4c8d426dec7aa
Piotr Chmielnik <piotr.chmielnik@eliatra.com>
2026-06-21 19:25:46 +0000

    Release FLX 4.1.0

--- tests/prepare_minikube.sh
+++ tests/prepare_minikube.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-VERSION=${1:-"v1.32.1"}
+VERSION=${1:-"v1.36.1"}
 killall -9 minikube
 killall -9 kubectl
 killall -9 helm
@@ -12,7 +12,7 @@ minikube config set memory 8192 -p "$PROFILE"
 minikube config set cpus 4 -p "$PROFILE"
 minikube delete -p "$PROFILE"
 set -e
-minikube start --kubernetes-version "$VERSION" --nodes 3 -p "$PROFILE" --wait=true
+minikube start --kubernetes-version "$VERSION"   --container-runtime=containerd  --nodes 3 -p "$PROFILE" --wait=true
 
 #fix minikube issues with hostpath permissions on multicluster nodes
 #https://github.com/kubernetes/minikube/issues/12165
